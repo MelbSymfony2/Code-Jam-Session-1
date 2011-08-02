2bfabe4e1f71b0f0b997dc042d0da67941fa7ece
Cameron Manderson <cameronmanderson@gmail.com>
2011-08-02 14:05:15 +1000

    Doctrine example: adding a simple entity
    Created a dummy entity named Product Then: php app/console
    doctrine:database:create php app/console doctrine:schema:create  Preview
    using PHPMyAdmin

---
+++ Symfony/src/MelbSymfony2/ForumExampleBundle/Entity/Product.php
@@ -0,0 +1,41 @@
+<?php
+/**
+ * Created by JetBrains PhpStORM.
+ * User: camm
+ * Date: 2/08/11
+ * Time: 9:47 AM
+ * To change this template use File | Settings | File Templates.
+ */
+
+namespace MelbSymfony2\ForumExampleBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+/**
+ * @ORM\Entity
+ * @ORM\Table(name="product")
+ */
+class Product
+{
+    /**
+     * @ORM\Id
+     * @ORM\Column(type="integer")
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    protected $id;
+
+    /**
+     * @ORM\Column(type="string", length=100)
+     */
+    protected $name;
+
+    /**
+     * @ORM\Column(type="decimal", scale=2)
+     */
+    protected $price;
+
+    /**
+     * @ORM\Column(type="text")
+     */
+    protected $description;
+
+}
