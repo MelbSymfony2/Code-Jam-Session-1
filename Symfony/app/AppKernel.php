de7e6981f65605bfe2dbaf9dcfbd26beeb4aebb9
Cameron Manderson <cameronmanderson@gmail.com>
2011-08-02 14:29:15 +1000

    Register the doctrine library in the autoload, and add the bundle to the app kernel

--- Symfony/app/AppKernel.php
+++ Symfony/app/AppKernel.php
@@ -18,6 +18,7 @@ class AppKernel extends Kernel
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new JMS\SecurityExtraBundle\JMSSecurityExtraBundle(),
             new MelbSymfony2\ForumExampleBundle\MelbSymfony2ForumExampleBundle(),
+            new Symfony\Bundle\DoctrineFixturesBundle\DoctrineFixturesBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
